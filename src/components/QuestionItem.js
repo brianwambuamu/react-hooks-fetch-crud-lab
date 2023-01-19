17ff0530050af70c35a871ddf2914036a53231ce
brianwambuamu <mutisobrian210@gmail.com>
2023-01-19 08:58:10 +0300

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question, questionNo, handleDeleteQuestion}) {
+  const { prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +9,41 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDeleteClick(e) {
+    // const questionId = e.target.parentElement.id
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+
+      }
+    })
+      .then(result => result.json())
+      .then(() => handleDeleteQuestion(question.id))
+  }
+
+  function handleEditClick(e) {
+    const newAnswer = parseInt(e.target.value)
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex: newAnswer })
+    })
+      .then(result => result.json())
+      .then((data) => console.log(question.id))
+  }
+
   return (
-    <li>
-      <h4>Question {id}</h4>
+    <li id={question.id}>
+      <h4>Question {questionNo}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex}onChange={handleEditClick}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
